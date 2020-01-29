d850c2f4bee8c2df7d9ddb2b2352c81a461ce3d2
nikkipeel <npeel3497@gmail.com>
2020-01-29 18:04:51 -0500

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import CoinContainer from "./CoinContainer";
+
+function App() {
+  return (
+    <div>
+      <CoinContainer />
+    </div>
+  );
+}
+
+export default App;
